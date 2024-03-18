9b43cf9452bd1fda86cd0d6f8ca692d0fd07e1d2
hritikchauhanji <147012445+hritikchauhanji@users.noreply.github.com>
2024-03-18 16:11:00 +0530

    datatypes

---
+++ b1/2Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not broswer
+
+console.log(3+3) //code reability should be high
+
+console.log("Hritik")
+
+let name = "hritik"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
