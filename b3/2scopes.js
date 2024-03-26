4e8c4a00710b7ed420e0f7111f645677daa3d286
hritikchauhanji <147012445+hritikchauhanji@users.noreply.github.com>
2024-03-26 10:38:19 +0530

    scopes

---
+++ b3/2scopes.js
@@ -0,0 +1,52 @@
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10 
+    const b = 20
+    var c = 30
+    // console.log("INNER: ", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "hritik"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    
+    // console.log(website);
+    two()
+}
+// console.log(username);
+
+one()
+
+if(true){
+    const username = "hritik"
+    if(username === "hritik"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// **************** interesting ************************
+
+console.log(addone(5));
+
+function addone(num){
+    return num +1 
+}
+
+const addTwo = function(num){
+    return num +2
+}
+addTwo(5)
