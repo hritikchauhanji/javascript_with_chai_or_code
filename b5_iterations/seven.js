31d1701ff77c095970c97f04dc3885c45e9d435a
hritikchauhanji <147012445+hritikchauhanji@users.noreply.github.com>
2024-03-31 17:46:13 +0530

    map&filter

---
+++ b5_iterations/seven.js
@@ -0,0 +1,11 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// const newnums = nums.map( (num) => {return num + 10})
+
+const newnums = nums
+                    .map((num) => num * 10)
+                    .map((num)=> num + 1)
+                    .filter((num) => () => 40)
+
+console.log(newnums);
