bc930e9de65790a66d706afb0214dd5f137b41d9
hritikchauhanji <147012445+hritikchauhanji@users.noreply.github.com>
2024-03-31 17:27:02 +0530

    filter

---
+++ b5_iterations/six.js
@@ -0,0 +1,36 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newnums = nums.filter( (num) => {
+//     return num > 4
+// })
+
+const newnums = []
+
+// nums.forEach( (num) => {
+//     if (num > 4) {
+//         newnums.push(num)
+//     }
+// })
+
+// console.log(newnums);
+
+// const nums2 = [11, 12, 13, 14, 15]
+
+// for (let index = 0; index < Array.length; index++) {
+//     newnums.push(nums)
+//     newnums.push(nums2)
+// }
+
+// const newnums2 = newnums.flat(Infinity)
+
+// console.log(newnums2);
+
